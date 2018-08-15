1d928bb9079aa20a24e7e79d4da2533baa49235a
guoyibin <guoyibin@suiyueyule.com>
2018-08-15 11:28:46 +0800

    init

---
+++ src/main/java/spring/boot/web/SpringBootWebApplication.java
@@ -0,0 +1,15 @@
+package spring.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guo
+ */
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
